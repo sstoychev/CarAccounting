e5b716ea04ec774ebbdcd53ae6d57e4e691fcc9e
Stoycho Stoychev <sstoychev@sstoychev.name>
2015-05-28 03:47:30 +0300

    initial

---
+++ app/src/main/java/name/sstoychev/caraccounting/CarAccounting.java
@@ -0,0 +1,90 @@
+package name.sstoychev.caraccounting;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class CarAccounting extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_accounting);
+        //String[] files = {"1", "2", "3"};
+        //String[] files = Environment.getRootDirectory().list();
+        String[] files = getFullPathFiles(Environment.getRootDirectory());
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,files);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.fileslistviewlayout,R.id.filesTextView,files);
+
+        filesDisplayAdapter arrayAdapter=new filesDisplayAdapter( this, R.layout.fileslistviewlayout, files);
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(arrayAdapter);
+    }
+
+    public String[] getFullPathFiles(File dir){
+        String[] files = dir.list();
+        String path = dir.getAbsolutePath();
+        for (int i = 0;  i < files.length; i++) {
+            files[i] = path+"/"+files[i];
+        }
+        return files;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_car_accounting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void browseForFiles(View view) {
+        String path = Environment.getExternalStorageDirectory().toString();
+
+        ListView l =(ListView)findViewById(R.id.listView);
+        /////////
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(getApplicationContext(), "Media mounted",
+                    Toast.LENGTH_LONG).show();
+        } else if  (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            Toast.makeText(getApplicationContext(), "Media mounted read only",
+                    Toast.LENGTH_LONG).show();
+        } else  {
+            Toast.makeText(getApplicationContext(), "Media not mounted",
+                    Toast.LENGTH_LONG).show();
+        }
+        //////
+        File dir = Environment.getRootDirectory();
+        File files[] = dir.listFiles();
+        /*Log.d("Files", "Size: "+ file.length);
+        for (int i=0; i < file.length; i++)
+        {
+            Log.d("Files", "FileName:" + file[i].getName());
+        }
+        Toast.makeText(getApplicationContext(), files.toString(),
+                Toast.LENGTH_LONG).show();
+         */
+    }
+}
---
+++ app/src/main/java/name/sstoychev/caraccounting/filesDisplayAdapter.java
@@ -0,0 +1,53 @@
+package name.sstoychev.caraccounting;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+
+/**
+ * Created by Admin on 25.5.2015 ã..
+ */
+public class filesDisplayAdapter extends ArrayAdapter<String>{
+
+    private Context context;
+    private int resource;
+    private String[] objects;
+    public filesDisplayAdapter(Context context, int resource, String[] objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        LayoutInflater inflater  = ((Activity) context).getLayoutInflater();
+        View row = inflater.inflate(resource, parent, false);
+        TextView filesTextView = (TextView) row.findViewById(R.id.filesTextView);
+        ImageView  imageView = (ImageView) row.findViewById(R.id.imageView);
+        CheckBox checkBox = (CheckBox) row.findViewById(R.id.checkBox);
+
+        filesTextView.setText((CharSequence) objects[position]);
+        File file = new File(objects[position]);
+        if (file.isDirectory()) {
+            //checkBox.setEnabled(false);
+            checkBox.setVisibility(View.INVISIBLE);
+            imageView.setImageResource(R.drawable.diricon);
+        }
+        else {
+            checkBox.setEnabled(true);
+            imageView.setImageResource(R.drawable.fileicon);
+        }
+        return row;
+    }
+}
